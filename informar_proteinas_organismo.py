b513803d1c958ef3f3c2f7ef1f8c77ca1ca64e37
Victor Pastor <victorpastorcaro@gmail.com>
2018-10-25 12:37:40 +0200

    Salida en documento externo 25-10

--- informar_proteinas_organismo.py
+++ informar_proteinas_organismo.py
@@ -1,7 +1,7 @@
 import sys
 import gzip
 #si pasan menos o mas de 2 argumentos, correccion de errores
-if (len(sys.argv)>2 or len(sys.argv)<2):
+if (len(sys.argv)>3 or len(sys.argv)<3):
 	print ('Numero de argumentos invalidos')
 else:
 	#inicializamos la variable adn para ir introduciendo datos en ella
@@ -11,6 +11,7 @@ else:
 	linea_nombre=""
 	contador = 1
 	fileadn = open(sys.argv[1])
+	filetoprint = open(sys.argv[2],"w")
 	for linea  in  fileadn:
 		#print (linea[0:2])
 		if (('OX') == (linea[0:2])):
@@ -18,11 +19,15 @@ else:
 			linea_prueba=linea_prueba[1].split('=')
 			linea_prueba=linea_prueba[1].replace(".", "").replace("\n", "").replace(";","")
 			if linea_nombre in array:
-				print array[linea_nombre]
+				#print array[linea_nombre]
 				array[linea_nombre][1]+=1
 			else:
 				array[linea_nombre]=[linea_prueba,contador]
-			print(contador,linea_prueba)
+			#print(contador,linea_prueba)
 		if (('OS') == (linea[0:2])):
 			linea_nombre = linea.split('   ')[1].replace(".", "").replace("\n", "")
 	print(array)
+	for i in array:
+		filetoprint.write(str(i))
+		# + str(i[0]) + str(i[1])
+	filetoprint.close()
