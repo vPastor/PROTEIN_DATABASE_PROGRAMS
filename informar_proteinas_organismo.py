2acbd851658013f82cf58a0749e2f73f0052361c
Victor Pastor <victorpastorcaro@gmail.com>
2018-10-29 10:15:36 +0100

    output_tabulado 29-10

--- informar_proteinas_organismo.py
+++ informar_proteinas_organismo.py
@@ -1,6 +1,7 @@
 import sys
 import gzip
-#si pasan menos o mas de 2 argumentos, correccion de errores
+from tabulate import tabulate
+#si pasan menos o mas de 3 argumentos, correccion de errores
 if (len(sys.argv)>3 or len(sys.argv)<3):
 	print ('Numero de argumentos invalidos')
 else:
@@ -10,24 +11,30 @@ else:
 	linea_prueba=""
 	linea_nombre=""
 	contador = 1
-	fileadn = open(sys.argv[1])
+	repite = 0
+	fileadn = gzip.open(sys.argv[1],"r")
 	filetoprint = open(sys.argv[2],"w")
+	array[0]=["TaxID","Scientific Name(Common Name)","Number of proteins"]
 	for linea  in  fileadn:
 		#print (linea[0:2])
 		if (('OX') == (linea[0:2])):
+			repite = 0
 			linea_prueba = linea.split('   ')
 			linea_prueba=linea_prueba[1].split('=')
 			linea_prueba=linea_prueba[1].replace(".", "").replace("\n", "").replace(";","")
 			if linea_nombre in array:
 				#print array[linea_nombre]
-				array[linea_nombre][1]+=1
+				array[linea_nombre][2]+=1
 			else:
-				array[linea_nombre]=[linea_prueba,contador]
+				array[linea_nombre]=[linea_nombre,linea_prueba,contador]
 			#print(contador,linea_prueba)
 		if (('OS') == (linea[0:2])):
-			linea_nombre = linea.split('   ')[1].replace(".", "").replace("\n", "")
+			linea_nombre_prov = linea.split('   ')[1].replace(".", "").replace("\n", "")
+			if repite == 1:
+                linea_nombre+=linea_nombre_prov
+            else:
+                linea_nombre =linea_nombre_prov
+                repite = 1
 	print(array)
-	for i in array:
-		filetoprint.write(str(i))
-		# + str(i[0]) + str(i[1])
+	filetoprint.write(tabulate(array.values(),headers="firstrow"))
 	filetoprint.close()
